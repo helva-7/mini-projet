c7104165c301befa3e6d029fa10c5c31f878b7c7
SGP <50177421+SGP07@users.noreply.github.com>
2023-12-16 19:36:52 +0100

    fixed refresh issue

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 import streamlit as st
 import streamlit_authenticator as stauth
+from time import sleep
 
 from utils import load_data, get_credentials, get_user_by_username
 from student import student_page
 from admin import admin_page
 
-@st.cache_data
-def _load_data():
-    return load_data()
 
-if "data" not in st.session_state:
-    st.session_state.data = _load_data()
+def main():
+    # if "data" not in st.session_state:
+    #     st.session_state.data = _load_data()
+    data = load_data()    
+    credentials = get_credentials(data)
 
-data = st.session_state.data
+    authenticator = stauth.Authenticate(credentials, 'user_dashboard', 'abcdef', cookie_expiry_days=1)
+
+    name, authentication_status, username = authenticator.login('Login', 'main')
+
+    if authentication_status:
+        authenticator.logout('Logout', 'main')
+        user, type = get_user_by_username(data, username)
+
+        if type == 'student':
+            student_page(user, data)
+        elif type == 'admin':   
+            admin_page(user, data)
+
+    elif authentication_status == False:
+        st.error('Username/password is incorrect')
+    elif authentication_status == None:
+        st.warning('Please enter your username and password')
 
 
-credentials = get_credentials(data)
+# @st.cache_data
+# def _load_data():
+#     return load_data()
 
-
-authenticator = stauth.Authenticate(credentials, 'user_dashboard', 'abcdef', cookie_expiry_days=1)
-
-name, authentication_status, username = authenticator.login('Login', 'main')
-
-
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    user, type = get_user_by_username(data, username)
-
-    if type == 'student':
-        student_page(user, data)
-    elif type == 'admin':   
-        admin_page(user, data)
-
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+if __name__ == "__main__":
+    main()
